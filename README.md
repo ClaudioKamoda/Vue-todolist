e830402909473f23bcbd8971020288f2f3448668
Claudio <claudiojlf@gmail.com>
2021-10-20 16:05:12 -0300

    adding item to list/using local storage

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
-# todolist
+# To-Do List
+
+How to setup the prohect on your machine:
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Project Log:
+
+#### October 20, 2021:
+
+-
---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="todo-list">
+    <h3>Todo List</h3>
+    <input type="text" v-on:keyup.enter="addItemToList">
+
+    <ul>
+      <li v-for="(item, index) in list" :key="index">
+        <input type="checkbox" v-model="item.checked">
+        {{item.text}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data(){
+    return{
+      list: []
+    }
+  },
+  created(){
+    const listOnMemory = JSON.parse(localStorage.getItem('list'));
+    
+    if(listOnMemory != null) //necessary check to see if it is the first time.
+      this.list = listOnMemory;
+  },
+  methods: {
+    addItemToList(event){
+      const newItem = event.target.value;
+      this.list.push({text: newItem, checked: false});
+      localStorage.setItem('list', JSON.stringify(this.list));
+      event.target.value = '';
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ul{
+    list-style: none;
+    padding: 0;
+  }
+</style>
